a0d916d905187a793b3b34d17da89460eea55ea3
Ha.Minh <minhhh@minhhuyha.info>
2017-10-18 02:05:44 +0700

    Initial commit

---
+++ Assets/Plugins/UnityHash/UnityHash.cs
@@ -0,0 +1,128 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using System.IO;
+
+namespace UnityHash
+{
+    public class HashUtil
+    {
+        protected static MD5 md5 = new System.Security.Cryptography.MD5CryptoServiceProvider ();
+        protected static RIPEMD160 ripemd160 = new System.Security.Cryptography.RIPEMD160Managed ();
+        protected static SHA1 sha1 = new System.Security.Cryptography.SHA1CryptoServiceProvider ();
+
+        protected static StringBuilder sb = new StringBuilder ();
+        protected const string X2 = "x2";
+
+        public static string MD5 (string input)
+        {
+            byte[] data = md5.ComputeHash (Encoding.UTF8.GetBytes (input));
+            sb.Length = 0;
+
+            for (int i = 0; i < data.Length; i++) {
+                sb.Append (data [i].ToString (X2));
+            }
+                
+            return sb.ToString ();
+        }
+
+        public static string MD5 (byte[] input)
+        {
+            byte[] data = md5.ComputeHash (input);
+            sb.Length = 0;
+
+            for (int i = 0; i < data.Length; i++) {
+                sb.Append (data [i].ToString (X2));
+            }
+
+            return sb.ToString ();
+        }
+
+        public static string MD5 (Stream input)
+        {
+            byte[] data = md5.ComputeHash (input);
+            sb.Length = 0;
+
+            for (int i = 0; i < data.Length; i++) {
+                sb.Append (data [i].ToString (X2));
+            }
+
+            return sb.ToString ();
+        }
+
+        public static string RIPEMD160 (string input)
+        {
+            byte[] data = ripemd160.ComputeHash (Encoding.UTF8.GetBytes (input));
+            sb.Length = 0;
+
+            for (int i = 0; i < data.Length; i++) {
+                sb.Append (data [i].ToString (X2));
+            }
+
+            return sb.ToString ();
+        }
+
+        public static string RIPEMD160 (byte[] input)
+        {
+            byte[] data = ripemd160.ComputeHash (input);
+            sb.Length = 0;
+
+            for (int i = 0; i < data.Length; i++) {
+                sb.Append (data [i].ToString (X2));
+            }
+
+            return sb.ToString ();
+        }
+
+        public static string RIPEMD160 (Stream input)
+        {
+            byte[] data = ripemd160.ComputeHash (input);
+            sb.Length = 0;
+
+            for (int i = 0; i < data.Length; i++) {
+                sb.Append (data [i].ToString (X2));
+            }
+
+            return sb.ToString ();
+        }
+
+        public static string SHA1 (string input)
+        {
+            byte[] data = sha1.ComputeHash (Encoding.UTF8.GetBytes (input));
+            sb.Length = 0;
+
+            for (int i = 0; i < data.Length; i++) {
+                sb.Append (data [i].ToString (X2));
+            }
+
+            return sb.ToString ();
+        }
+
+        public static string SHA1 (byte[] input)
+        {
+            byte[] data = sha1.ComputeHash (input);
+            sb.Length = 0;
+
+            for (int i = 0; i < data.Length; i++) {
+                sb.Append (data [i].ToString (X2));
+            }
+
+            return sb.ToString ();
+        }
+
+        public static string SHA1 (Stream input)
+        {
+            byte[] data = sha1.ComputeHash (input);
+            sb.Length = 0;
+
+            for (int i = 0; i < data.Length; i++) {
+                sb.Append (data [i].ToString (X2));
+            }
+
+            return sb.ToString ();
+        }
+
+    }
+
+}
---
+++ Assets/Tests/Editor/UnityHashTest.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using UnityHash;
+using System.Text;
+using System.IO;
+
+public class UnityHashTest
+{
+    string sampleString1 = "The quick brown fox jumps over the lazy dog";
+
+    public static Stream GenerateStreamFromString(string s)
+    {
+        MemoryStream stream = new MemoryStream();
+        StreamWriter writer = new StreamWriter(stream);
+        writer.Write(s);
+        writer.Flush();
+        stream.Position = 0;
+        return stream;
+    }
+
+    [SetUp]
+    public void Setup ()
+    {
+    }
+
+    [Test]
+    public void ReturnCorrectMd5String ()
+    {
+        Assert.AreEqual ("9e107d9d372bb6826bd81d3542a419d6", HashUtil.MD5 (sampleString1));
+    }
+
+    [Test]
+    public void ReturnCorrectMd5ByteArray ()
+    {
+        Assert.AreEqual ("9e107d9d372bb6826bd81d3542a419d6", HashUtil.MD5 (Encoding.UTF8.GetBytes(sampleString1)));
+    }
+
+    [Test]
+    public void ReturnCorrectMd5Stream ()
+    {
+        using (var stream = GenerateStreamFromString (sampleString1)) {
+            Assert.AreEqual ("9e107d9d372bb6826bd81d3542a419d6", HashUtil.MD5 (stream));
+        }
+    }
+
+    [Test]
+    public void ReturnCorrectRIPEMD160String ()
+    {
+        Assert.AreEqual ("37f332f68db77bd9d7edd4969571ad671cf9dd3b", HashUtil.RIPEMD160 (sampleString1));
+    }
+
+    [Test]
+    public void ReturnCorrectRIPEMD160ByteArray ()
+    {
+        Assert.AreEqual ("37f332f68db77bd9d7edd4969571ad671cf9dd3b", HashUtil.RIPEMD160 (Encoding.UTF8.GetBytes(sampleString1)));
+    }
+
+    [Test]
+    public void ReturnCorrectRIPEMD160Stream ()
+    {
+        using (var stream = GenerateStreamFromString (sampleString1)) {
+            Assert.AreEqual ("37f332f68db77bd9d7edd4969571ad671cf9dd3b", HashUtil.RIPEMD160 (stream));
+        }
+    }
+
+    [Test]
+    public void ReturnCorrectSHA1String ()
+    {
+        Assert.AreEqual ("2fd4e1c67a2d28fced849ee1bb76e7391b93eb12", HashUtil.SHA1 (sampleString1));
+    }
+
+    [Test]
+    public void ReturnCorrectSHA1ByteArray ()
+    {
+        Assert.AreEqual ("2fd4e1c67a2d28fced849ee1bb76e7391b93eb12", HashUtil.SHA1 (Encoding.UTF8.GetBytes(sampleString1)));
+    }
+
+
+    [Test]
+    public void ReturnCorrectSHA1Stream ()
+    {
+        using (var stream = GenerateStreamFromString (sampleString1)) {
+            Assert.AreEqual ("2fd4e1c67a2d28fced849ee1bb76e7391b93eb12", HashUtil.SHA1 (stream));
+        }
+    }
+}
